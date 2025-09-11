c284cb5c8590f6d510cafa0d60bbcf45c2f3cbda
Akila <akilaewis@gmail.com>
2025-09-11 20:16:38 +0530

    added service and serviceImpl

--- ecommerce/src/main/java/com/ewis/ecommerce/controller/CategoryController.java
+++ ecommerce/src/main/java/com/ewis/ecommerce/controller/CategoryController.java
@@ -1,6 +1,8 @@
 package com.ewis.ecommerce.controller;
 
 import com.ewis.ecommerce.model.Category;
+import com.ewis.ecommerce.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -9,16 +11,17 @@ import java.util.List;
 @RestController
 public class CategoryController {
 
-    private List <Category> categories = new ArrayList<>();
+    @Autowired
+    private CategoryService categoryService;
 
     @GetMapping("/api/public/categories")
     public List <Category> getAllCategories(){
-        return categories;
+        return categoryService.getAllCategories();
     }
 
     @PostMapping("/api/public/categories")
     public String createCategory(@RequestBody Category category){
-        categories.add(category);
-        return "Category added sucsessfully";
+        categoryService.createCategory(category);
+        return "Category added successfully";
     }
 }
