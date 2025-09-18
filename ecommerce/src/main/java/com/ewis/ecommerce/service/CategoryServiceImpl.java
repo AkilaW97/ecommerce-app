74b65cb0e8ab75893cda081538dbbf68ed235104
Akila <akilaewis@gmail.com>
2025-09-18 21:13:56 +0530

    added repository methods to remove boiler plate code

--- ecommerce/src/main/java/com/ewis/ecommerce/service/CategoryServiceImpl.java
+++ ecommerce/src/main/java/com/ewis/ecommerce/service/CategoryServiceImpl.java
@@ -4,6 +4,7 @@ import com.ewis.ecommerce.model.Category;
 import com.ewis.ecommerce.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -27,35 +28,29 @@ public class CategoryServiceImpl implements CategoryService{
 
     @Override
     public void createCategory(Category category) {
-        category.setCategoryId(nextId++);
+        //category.setCategoryId(nextId++);
         categoryRepository.save(category);
     }
 
     @Override
     public String deleteCategory(Long categoryId) {
 
-        List<Category> categories = categoryRepository.findAll();
-        Category category = categories.stream().filter(c -> c.getCategoryId().equals(categoryId))
-                .findFirst().orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Resource Not found"));
+        Category deleteCategory = categoryRepository.findById(categoryId).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Category is Not found"));
 
-        categoryRepository.delete(category);
-        return "Category with categoryId: " + categoryId +" deleted successfully";
+       categoryRepository.delete(deleteCategory);
+       return "Deleted category id " + categoryId + " Successfully";
     }
 
     @Override
     public Category updateCategory(Category category, Long categoryId) {
-        List<Category> categories = categoryRepository.findAll();
 
-        Optional<Category> optionalCategory = categories.stream().filter(c -> c.getCategoryId().equals(categoryId)).findFirst();
+        Category savedCategory = categoryRepository.findById(categoryId).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Resource Not found"));
 
-        if(optionalCategory.isPresent()){
-            Category exsistingCategory = optionalCategory.get();
-            exsistingCategory.setCategoryName(category.getCategoryName());
-            Category savedCategory =  categoryRepository.save(exsistingCategory);
-            return savedCategory;
-        }else{
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Category Not found");
-        }
+        // Copy only the fields that are allowed to change
+        savedCategory.setCategoryName(category.getCategoryName());
+
+        // Because savedCategory is managed by Hibernate, save() will issue an UPDATE
+        return categoryRepository.save(savedCategory);
     }
 
 
