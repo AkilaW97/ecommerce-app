99faf86acca1cb24562d4f72947f53c156a87057
Akila <akilaewis@gmail.com>
2025-10-29 09:48:11 +0530

    created the add prodduct endpoint with the implementation and add image and specialPrice to the product entitiy

--- ecommerce/src/main/java/com/ewis/ecommerce/service/ProductService.java
+++ ecommerce/src/main/java/com/ewis/ecommerce/service/ProductService.java
@@ -1,4 +1,8 @@
 package com.ewis.ecommerce.service;
 
+import com.ewis.ecommerce.model.Product;
+import com.ewis.ecommerce.payload.ProductDto;
+
 public interface ProductService {
+    ProductDto addProduct(Long categoryId, Product product);
 }
