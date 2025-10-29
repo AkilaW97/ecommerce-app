5f35ec7394b85a448a4924e4a70ebdf0f91c312f
Akila <akilaewis@gmail.com>
2025-10-29 19:41:21 +0530

    Added get all products

--- ecommerce/src/main/java/com/ewis/ecommerce/service/ProductService.java
+++ ecommerce/src/main/java/com/ewis/ecommerce/service/ProductService.java
@@ -2,7 +2,10 @@ package com.ewis.ecommerce.service;
 
 import com.ewis.ecommerce.model.Product;
 import com.ewis.ecommerce.payload.ProductDto;
+import com.ewis.ecommerce.payload.ProductResponse;
 
 public interface ProductService {
     ProductDto addProduct(Long categoryId, Product product);
+
+    ProductResponse getAllProducts();
 }
