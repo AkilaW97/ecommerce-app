212c3f5aebd91fbb46ce7d26b21ae8d6fa3c75d6
Akila <akilaewis@gmail.com>
2025-11-02 21:09:05 +0530

    added products get by category

--- ecommerce/src/main/java/com/ewis/ecommerce/repository/ProductRepository.java
+++ ecommerce/src/main/java/com/ewis/ecommerce/repository/ProductRepository.java
@@ -1,10 +1,14 @@
 package com.ewis.ecommerce.repository;
 
+import com.ewis.ecommerce.model.Category;
 import com.ewis.ecommerce.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
+    List<Product> findByCategoryOrderByPriceAsc(Category category);
 }
